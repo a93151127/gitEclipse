7854726915be5724ca45e80cc28d4d2c9eb7585e
martinlu <a93151127@gmail.com>
2022-08-10 13:21:51 +0800

    create spring update

---
+++ SpringDemoUpdate/src/main/java/com/demo/martin/SpringDemoUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.demo.martin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoUpdateApplication.class, args);
+	}
+
+}
---
+++ SpringDemoUpdate/src/main/java/com/demo/martin/controller/SpringMartinController.java
@@ -0,0 +1,13 @@
+package com.demo.martin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringMartinController {
+	
+	@RequestMapping("/")
+	public String helloWorld() {
+		return "hello world";
+	}
+}
